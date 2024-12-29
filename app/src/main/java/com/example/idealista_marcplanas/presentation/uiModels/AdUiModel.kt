9f0058f6cbc13d6160009d29a786af747b9d2f08
Marc Planas <planasmultimedia@gmail.com>
2024-12-29 13:43:29 +0100

    New Design: Complete Ad details on the List Screen

--- app/src/main/java/com/example/idealista_marcplanas/presentation/uiModels/AdUiModel.kt
+++ app/src/main/java/com/example/idealista_marcplanas/presentation/uiModels/AdUiModel.kt
@@ -2,7 +2,12 @@ package com.example.idealista_marcplanas.presentation.uiModels
 
 data class AdUiModel(
     val id: String,
+    val title : String,
     val address: String,
     val price: String,
-    val thumbnail: String
+    val parkingInfo : String,
+    val images: List<String>,
+    val roomInfo: String,
+    val sizeInfo: String,
+    val operation : String
 )
